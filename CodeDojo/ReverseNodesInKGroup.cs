8b4c589a0fc33d81ad24b9e312158e20ad467cdb
vqt <vietqtrang@gmail.com>
2021-05-19 21:39:13 -0400

    Add project files

---
+++ CodeDojo/ReverseNodesInKGroup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeDojo
+{
+    public class ReverseNodesInKGroup
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            if (head == null) return head;
+            var stacks = new List<Stack<ListNode>>();
+            var stack = new Stack<ListNode>();
+            int count = 0;
+            while (head != null)
+            {
+                count++;
+                stack.Push(head);
+                if (count == k)
+                {
+                    count = 0;
+                    stacks.Add(new Stack<ListNode>(stack.Reverse()));
+                    stack.Clear();
+                }
+                head = head.next;
+            }
+            if(stack.Count > 0) stacks.Add(new Stack<ListNode>(stack));
+            head = stacks[0].Peek();
+            for (int i = 0; i < stacks.Count; i++)
+            {
+                stack = stacks[i];
+                if (stack.Count == k)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        stack.Pop().next = j < k - 1 ? stack.Peek() :
+                            i < stacks.Count - 1 ? stacks[i + 1].Peek() : null;
+                    }
+                }
+            }
+            return head;
+        }
+    }
+}
