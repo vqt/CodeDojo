520f88e13c0572ee7002fc5cdfedb007fe221ede
vqt <vietqtrang@gmail.com>
2021-07-08 19:31:31 -0400

    Maximum Length of Repeated Subarray

---
+++ CodeDojo/MaximumLengthOfRepeatedSubarray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDojo
+{
+    class MaximumLengthOfRepeatedSubarray
+    {
+        public int FindLength(int[] nums1, int[] nums2)
+        {
+            int max = 0;
+            int[,] matrix = new int[nums1.Length + 1, nums2.Length + 1];
+            for (int i = 0; i < nums1.Length; i++)
+                for (int j = 0; j < nums2.Length; j++)
+                    if (nums1[i] == nums2[j])
+                    {
+                        matrix[i + 1, j + 1] = matrix[i, j] + 1;
+                        max = Math.Max(max, matrix[i + 1, j + 1]);
+                    }
+            return max;
+        }
+    }
+}
