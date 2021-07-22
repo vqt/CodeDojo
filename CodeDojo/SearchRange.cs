72ca62afead8c71bb55cad7d42ac85e984d7895f
vqt <vietqtrang@gmail.com>
2021-07-21 22:22:19 -0400

    Add SearchRange

---
+++ CodeDojo/SearchRange.cs
@@ -0,0 +1,36 @@
+﻿namespace CodeDojo
+{
+    public partial class Solution
+    {
+        // Find First and Last Position of Element in Sorted Array
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var left = BinarySearchL(nums, target);
+            if (left == nums.Length || nums[left] != target) return new int[] { -1, -1 };
+            var right = BinarySearchR(nums, target);
+            return new int[] { left, right };
+        }
+        public int BinarySearchL(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length;
+            while (left < right)
+            {
+                var mid = left + (right - left) / 2;
+                if (nums[mid] >= target) right = mid; else left = mid + 1;
+            }
+            return left;
+        }
+        public int BinarySearchR(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length;
+            while (left < right)
+            {
+                var mid = left + (right - left) / 2;
+                if (nums[mid] > target) right = mid; else left = mid + 1;
+            }
+            return --right;
+        }
+    }
+}
